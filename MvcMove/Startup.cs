b69be838d2d76cf431aedeb21ccce1365c218379
MarcioHCL <marciotorres.rs@gmail.com>
2015-07-24 09:41:13 -0300

    Primeiro Commit

---
+++ MvcMove/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MvcMove.Startup))]
+namespace MvcMove
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
